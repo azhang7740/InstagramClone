45d2a00b21b6dce0df4345a4e125b8ad3bf68bbc
Angelina Zhang <azhang7740@gmail.com>
2022-06-27 10:46:06 -0700

    Added assets and connected app to Parse

--- InstagramClone/AppDelegate.m
+++ InstagramClone/AppDelegate.m
@@ -6,6 +6,7 @@
 //
 
 #import "AppDelegate.h"
+#import <Parse/Parse.h>
 
 @interface AppDelegate ()
 
@@ -15,7 +16,15 @@
 
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-    // Override point for customization after application launch.
+    ParseClientConfiguration *config = [ParseClientConfiguration
+                                        configurationWithBlock:^(id<ParseMutableClientConfiguration> configuration) {
+        configuration.applicationId = @"m7Ab3WKhFVGDpVGD9WcPLIw362IQbROABrHF7tsY";
+        configuration.clientKey = @"DSKN4zRFc63AGZBtlk1B1nbBISmhVL6JBPjA2opU";
+        configuration.server = @"https://parseapi.back4app.com";
+    }];
+
+    [Parse initializeWithConfiguration:config];
+    
     return YES;
 }
 
