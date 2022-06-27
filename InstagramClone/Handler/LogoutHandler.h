14eebad0b9a96ea0e53ceafc7d25175a8255a119
Angelina Zhang <azhang7740@gmail.com>
2022-06-27 16:04:32 -0700

    Implemented logout button

---
+++ InstagramClone/Handler/LogoutHandler.h
@@ -0,0 +1,19 @@
+//
+//  LogoutHandler.h
+//  InstagramClone
+//
+//  Created by Angelina Zhang on 6/27/22.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface LogoutHandler : NSObject
+
+- (instancetype)init;
+- (void)logout;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ InstagramClone/Handler/LogoutHandler.m
@@ -0,0 +1,29 @@
+//
+//  LogoutHandler.m
+//  InstagramClone
+//
+//  Created by Angelina Zhang on 6/27/22.
+//
+
+#import "LogoutHandler.h"
+#import "SceneDelegate.h"
+#import "LoginViewController.h"
+#import <Parse/Parse.h>
+
+@implementation LogoutHandler
+
+- (instancetype)init {
+    self = [super init];
+    return self;
+}
+
+- (void)logout {
+    [PFUser logOutInBackgroundWithBlock:^(NSError * _Nullable error) {}];
+    SceneDelegate *sceneDelegate = (SceneDelegate * ) UIApplication.sharedApplication.connectedScenes.allObjects.firstObject.delegate;
+    
+    UIStoryboard *storyboard = [UIStoryboard storyboardWithName:@"Main" bundle:nil];
+    LoginViewController *loginViewController = [storyboard instantiateViewControllerWithIdentifier:@"LoginViewController"];
+    sceneDelegate.window.rootViewController = loginViewController;
+}
+
+@end
