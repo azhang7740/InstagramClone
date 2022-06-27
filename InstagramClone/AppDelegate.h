ed7f2f94a809f6dd0f19effe47faf37952fdfdfc
Angelina Zhang <azhang7740@gmail.com>
2022-06-27 10:28:43 -0700

    Initial Commit

---
+++ InstagramClone/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  InstagramClone
+//
+//  Created by Angelina Zhang on 6/27/22.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
