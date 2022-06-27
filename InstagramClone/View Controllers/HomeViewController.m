edc25d9cbace1d1073fdea20635c8bd1748fae20
Angelina Zhang <azhang7740@gmail.com>
2022-06-27 16:40:31 -0700

    Created PostCell class with PostCell UI elements

--- InstagramClone/View Controllers/HomeViewController.m
+++ InstagramClone/View Controllers/HomeViewController.m
@@ -8,7 +8,9 @@
 #import "HomeViewController.h"
 #import "LogoutHandler.h"
 
-@interface HomeViewController ()
+@interface HomeViewController () <UITableViewDataSource, UITableViewDelegate>
+
+@property (weak, nonatomic) IBOutlet UITableView *timelineTableView;
 
 @end
 
@@ -16,11 +18,26 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    
+    self.timelineTableView.delegate = self;
+    self.timelineTableView.dataSource = self;
 }
 
 - (IBAction)onTapLogout:(id)sender {
     LogoutHandler *logoutAction = [[LogoutHandler alloc] init];
     [logoutAction logout];
+}
+
+- (nonnull UITableViewCell *)tableView:(nonnull UITableView *)tableView cellForRowAtIndexPath:(nonnull NSIndexPath *)indexPath {
+    return nil;
+}
+
+- (NSInteger)tableView:(nonnull UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return 0;
+}
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
+    
 }
 
 @end
