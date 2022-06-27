7aee42468caa105797475a7d1f7bf52620772f7e
Angelina Zhang <azhang7740@gmail.com>
2022-06-27 16:57:38 -0700

    Added ComposeViewController and ComposeView

---
+++ InstagramClone/View Controllers/ComposeViewController.m
@@ -0,0 +1,21 @@
+//
+//  ComposeViewController.m
+//  InstagramClone
+//
+//  Created by Angelina Zhang on 6/27/22.
+//
+
+#import "ComposeViewController.h"
+
+@interface ComposeViewController ()
+
+@end
+
+@implementation ComposeViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+@end
