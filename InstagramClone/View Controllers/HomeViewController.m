8f03d971a13a3608025b69f3b6b1be8ae1eb3fc3
Angelina Zhang <azhang7740@gmail.com>
2022-06-28 11:53:33 -0700

    Implemented table view for posts to load in

--- InstagramClone/View Controllers/HomeViewController.m
+++ InstagramClone/View Controllers/HomeViewController.m
@@ -8,10 +8,14 @@
 #import "HomeViewController.h"
 #import "LogoutHandler.h"
 #import "ComposeViewController.h"
+#import "PostCell.h"
+
+#import "Post.h"
 
 @interface HomeViewController () <UITableViewDataSource, UITableViewDelegate, ComposeViewControllerDelegate>
 
 @property (weak, nonatomic) IBOutlet UITableView *timelineTableView;
+@property (nonatomic, strong) NSArray<Post *> *posts;
 
 @end
 
@@ -22,6 +26,28 @@
     
     self.timelineTableView.delegate = self;
     self.timelineTableView.dataSource = self;
+    [self fetchPosts];
+    
+    UIRefreshControl *refreshControl = [[UIRefreshControl alloc] init];
+    [refreshControl addTarget:self
+                       action:@selector(beginRefresh:)
+             forControlEvents:UIControlEventValueChanged];
+    [self.timelineTableView insertSubview:refreshControl atIndex:0];
+}
+
+- (void)fetchPosts {
+    PFQuery *query = [PFQuery queryWithClassName:@"Post"];
+    [query orderByDescending:@"createdAt"];
+    query.limit = 20;
+
+    [query findObjectsInBackgroundWithBlock:^(NSArray *posts, NSError *error) {
+        if (posts != nil) {
+            self.posts = posts;
+            [self.timelineTableView reloadData];
+        } else {
+            
+        }
+    }];
 }
 
 - (IBAction)onTapLogout:(id)sender {
@@ -40,20 +66,46 @@
     [self dismissViewControllerAnimated:YES completion:nil];
 }
 
-- (void)didTapShare {
-    
+- (void)didTapShare:(UIImage *)image
+        withCaption:(NSString *)caption {
+    [Post postUserImage:image withCaption:caption withCompletion:^(BOOL succeeded, NSError * _Nullable error) {
+        if (error) {
+            
+        } else {
+            [self dismissViewControllerAnimated:YES completion:nil];
+        }
+    }];
 }
 
-- (nonnull UITableViewCell *)tableView:(nonnull UITableView *)tableView cellForRowAtIndexPath:(nonnull NSIndexPath *)indexPath {
-    return nil;
+- (nonnull UITableViewCell *)tableView:(nonnull UITableView *)tableView
+                 cellForRowAtIndexPath:(nonnull NSIndexPath *)indexPath {
+    PostCell *cell = [tableView dequeueReusableCellWithIdentifier:@"PostCell"
+                                                      forIndexPath:indexPath];
+    if (indexPath.row < self.posts.count) {
+        PFFileObject *image = self.posts[indexPath.row].image;
+        [image getDataInBackgroundWithBlock:^(NSData * _Nullable data, NSError * _Nullable error) {
+            if (!error) {
+                cell.postImage.image = [UIImage imageWithData:data];
+            }
+        }];
+        cell.captionTextView.text = self.posts[indexPath.row].caption;
+    }
+    
+    return cell;
 }
 
 - (NSInteger)tableView:(nonnull UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
-    return 0;
+    return self.posts.count;
 }
 
-- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
+- (void)tableView:(UITableView *)tableView
+didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
     
+}
+
+- (void)beginRefresh:(UIRefreshControl *)refreshControl {
+    [self fetchPosts];
+    [refreshControl endRefreshing];
 }
 
 @end
